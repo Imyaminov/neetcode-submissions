c28fbec7c5e36e81bfe10bbe16a86d1a41eb45e8
Abduboriy <93596148+Imyaminov@users.noreply.github.com>
2026-07-09 10:55:37 +0500

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+        for item in strs:
+            result += item+str(len(item))[-1]+"&"
+        print(result)
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        result = []             
+        element = ""
+
+        for idx in range(len(s)):
+            if s[idx] == "&" and s[idx-1].isdigit():
+                print(element, element[:-1])
+                element = element[:-1]
+                result.append(element)
+                element = ""
+                continue
+            element += s[idx]
+        return result
